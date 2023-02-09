b16ea33c727d0dd08ce1b53f035e98496f92d6a7
zhaolei <im.zhaolei@foxmail.com>
2023-02-09 15:52:25 +0800

    [builder] 02-create-builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,39 @@
-use proc_macro::TokenStream;
+use proc_macro::{Span, TokenStream};
+use quote::quote;
+use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Ident, Type};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+    impl_builder(&input)
+}
 
-    TokenStream::new()
+fn impl_builder(input: &DeriveInput) -> TokenStream {
+    let struct_name = &input.ident;
+    let builder_name = Ident::new(&format!("{}Builder", struct_name), Span::call_site().into());
+
+    let fields = match &input.data {
+        Data::Struct(DataStruct { fields, .. }) => fields,
+        _ => panic!("not a struct"),
+    };
+    let field_ident = fields
+        .iter()
+        .map(|field| &field.ident)
+        .collect::<Vec<&Option<Ident>>>();
+    let field_type = fields.iter().map(|field| &field.ty).collect::<Vec<&Type>>();
+
+    quote!(
+        pub struct #builder_name {
+            #(#field_ident: Option<#field_type>),*
+        }
+
+        impl #struct_name {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    #(#field_ident: None,)*
+                }
+            }
+        }
+    )
+    .into()
 }
